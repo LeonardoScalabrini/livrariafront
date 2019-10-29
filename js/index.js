4d2f57ec220933b2fa8253bd360d1637c5bf6e21
Leonardo <leonardo_scalabrini@hotmail.com>
2019-10-29 18:39:11 -0300

    Implementado front end livraria

--- js/index.js
+++ js/index.js
@@ -1,28 +1,50 @@
 var app = new Vue({
     el: '#app',
     data: {
-        livros: [
-            { titulo: 'Aprender JavaScript', autor: 'Lewis Carrol' },
-            { titulo: 'Aprender Vue', autor: 'Lewis Carrol' },
-            { titulo: 'Criar algo incrível', autor: 'Lewis Carrol' }
-        ],
+        livros: [],
         titulo: '',
         autor: '',
+        filtro: '',
         ehEditar: false,
         selecionado: null
     },
     methods: {
         adicionar: function (titulo, autor) {
-            this.livros.push({ titulo: titulo, autor: autor });
-            this.titulo = '';
-            this.autor = '';
+            Vue.http.post("http://localhost:8080/livraria", { titulo: titulo, autor: autor })
+                .then(response => {
+                    this.livros.push(response.body);
+                    this.titulo = '';
+                    this.autor = '';
+                    Swal.fire('Sucesso!', 'Livro adicionado com sucesso!', 'success');
+                }, response => {
+                    Swal.fire('Oops!', 'Algo deu errado!', 'error');
+                });
         },
         editar: function (titulo, autor) {
-            this.selecionado.titulo = titulo;
-            this.selecionado.autor = autor;
-            this.cancelar();
+            Vue.http.put("http://localhost:8080/livraria", { id: this.selecionado.id, titulo: titulo, autor: autor })
+                .then(response => {
+                    this.selecionado.titulo = titulo;
+                    this.selecionado.autor = autor;
+                    this.cancelar();
+                    Swal.fire('Sucesso!', 'Livro editado com sucesso!', 'success');
+                }, response => {
+                    Swal.fire('Oops!', 'Algo deu errado!', 'error');
+                });
         },
-        cancelar: function(){
+        buscar: function (filtro) {
+            Vue.http.get("http://localhost:8080/livraria/" + filtro)
+                .then(response => {
+                    this.$nextTick(() => {
+                        this.livros = response.body;
+                        
+                        if (response.body.length == 0)
+                            Swal.fire('Oops!', 'Nenhum livro foi encontrado!', 'success');
+                    });
+                }, response => {
+                    Swal.fire('Oops!', 'Algo deu errado!', 'error');
+                });
+        },
+        cancelar: function () {
             this.titulo = '';
             this.autor = '';
             this.selecionado = null;
@@ -38,8 +60,14 @@ var app = new Vue({
                 this.ehEditar = true;
             });
         },
-        remover: function (index) {
-            this.livros.splice(index, 1);
+        remover: function (index, livro) {
+            Vue.http.delete("http://localhost:8080/livraria/" + livro.id)
+                .then(response => {
+                    this.livros.splice(index, 1);
+                    Swal.fire('Sucesso!', 'Livro removido com sucesso!', 'success');
+                }, response => {
+                    Swal.fire('Oops!', 'Algo deu errado!', 'error');
+                });
         }
     }
 })
